6109334ee19e683b374f82453331d30f7571cea8
JinGoo <zzgg3040@naver.com>
2025-11-23 20:21:11 +0900

    init

---
+++ src/main/java/org/gooserver/GooServerApplication.java
@@ -0,0 +1,13 @@
+package org.gooserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GooServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gooserver/GooServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gooserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
